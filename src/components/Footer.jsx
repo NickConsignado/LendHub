c21713aa944129f4eca0782653e0f85222092dbb
Mark Dave <markdave.seneres@gamil.com>
2023-08-09 13:15:02 +0800

    adding footer content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,31 +3,55 @@ import { Link } from "react-router-dom"
 function Footer() {
   return (
     <>
-      <div>
-        <div className="d-flex justify-content-center text-align-center align-item-center mt-5">
-            <small>@2023 LendHub</small><br /><br />
+    <div style={{ backgroundImage: "linear-gradient(79deg, #35d9df, #7bddac, #397fdb)"}}>
+      <div className="container mt-5 pt-5 text-white">
+        <div className="row">
+          <div className="col-5">
+            <h2>Logo</h2>
+            <p>LendHub is the largest marketplace for users resources,
+                powered by a community of educators.
+            </p> 
+            <div className="d-flex justify-content-evenly pt-5">
+            <i class="fa-brands fa-facebook fa-xl"></i>   
+            <i class="fa-brands fa-instagram fa-xl"></i>  
+            <i class="fa-brands fa-pinterest fa-xl"></i> 
+            <i class="fa-brands fa-twitter fa-xl"></i> 
+            </div>  
+          </div>  
+      
+          <div className="col-2">
+            <h2>ABOUT</h2>
+            <p>Who We Are</p>
+            <p>We're Hiring</p>   
+            <p>Press</p>
+            <p>Blog</p>      
+          </div>  
+        
+          <div className="col-2">
+            <h2>SUPPORT</h2> 
+            <p>Help & FAQ</p>
+            <p>Privacy Policy</p>   
+            <p>Users Privacy</p>
+            <p>Terms of Service</p>                
+          </div>  
+       
+          <div className="col-3">
+            <h2>UPDATES</h2>
+            <p>Get our weekly newslatter with free
+              resoures, updates, and special
+              offers.
+            </p>          
+          </div>  
         </div>
-        <div className="d-flex justify-content-center text-align-center align-item-center">         
-            <p>Used API: <span className="text-decoration-underline">Book Borrowing REST API</span></p>               
-        </div>
-        <div className="d-flex justify-content-center text-align-center align-item-center">
-            <p>Develop by:</p>
-        </div>   
-        <div className="d-flex justify-content-center text-align-center align-item-center">        
-            <p>Nick Ilano Consignado <i class="fa-solid fa-minus fa-rotate-90"></i>
-            Banez Ederick Salcela</p>               
-        </div> 
-        <div className="d-flex justify-content-center text-align-center align-item-center">
-            <p>Mark Dave Seneres</p>             
+        <div className="container">
+          <hr />
+          <small className="d-flex justify-content-center mt-3 mb-3">Copyright © 2023 LendHub</small>
         </div>  
-        <br />
-        <div className="d-flex justify-content-center text-align-center align-item-center py-2">
-          <i class="fa-brands fa-github fa-xl"></i>
-          <i class="fa-brands fa-linkedin fa-xl"></i>
-        </div>      
-      </div>                        
+      </div>  
+    </div>         
     </>
   )
 }
 
 export default Footer;
+ 
