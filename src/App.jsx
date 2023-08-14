2cd9c33db3c794d2cee8c21e3fbfa12ff25bf9a7
Mark Dave <markdave.seneres@gamil.com>
2023-08-14 15:53:28 +0800

    import a provider

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,21 @@ import BookInfo from "./pages/BookInfo";
 import Footer from "./components/Footer";
 import BorrowedList from "./pages/BorrowedList";
 import "bootstrap/dist/js/bootstrap.js";
+import { Provider } from "react-redux";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/book-info" element={<BookInfo />} />
-          <Route path="/borrowed-list" element={<BorrowedList />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+      <Provider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/book-info" element={<BookInfo />} />
+            <Route path="/borrowed-list" element={<BorrowedList />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </Provider>
     </>
   );
 }
